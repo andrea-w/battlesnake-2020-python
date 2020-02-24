c8783a56c2d278d9779c24dff650395df28c0e12
Andrea <andrea.k.williams7@gmail.com>
2020-02-23 17:29:48 -0800

    Comment out print statements

--- app/main.py
+++ app/main.py
@@ -12,7 +12,7 @@ class FloodNode:
         self.colour = colour
 
     def to_string(self):
-        print('(' + str(self.position.x) + ',' + str(self.position.y) + ') - ' + self.colour)
+        # print('(' + str(self.position.x) + ',' + str(self.position.y) + ') - ' + self.colour)
 
 class FloodGrid:
     def __init__(self, height, width):
@@ -21,10 +21,10 @@ class FloodGrid:
         self.grid = [[FloodNode(Position(x,y)) for x in range(width)] for y in range(height)]
 
     def insert(self, node):
-        if (node.position.x < 0 or node.position.x > self.width):
-            print('index out of range at x=' + str(node.position.x))
-        if (node.position.y < 0 or node.position.y > self.height):
-            print('index out of range at y=' + str(node.position.y))
+        # if (node.position.x < 0 or node.position.x > self.width):
+        #     print('index out of range at x=' + str(node.position.x))
+        # if (node.position.y < 0 or node.position.y > self.height):
+        #     print('index out of range at y=' + str(node.position.y))
         self.grid[node.position.x][node.position.y] = node
 
     def print_string(self):
@@ -47,7 +47,7 @@ class FloodGrid:
         if (x >= 0 and x < self.width and y >= 0 and y < self.height):
             return self.grid[y][x]
         else:
-            print('grid index out of range for ' + str(x) + ',' + str(y))
+            # print('grid index out of range for ' + str(x) + ',' + str(y))
             return
 
     def count_red(self):
@@ -142,22 +142,22 @@ def check_for_obstacle(data, position):
 def flood_fill_left(node, target_colour, replace_colour):
     global floodGrid
     if (floodGrid is None):
-        print('error retrieving floodGrid')
+        # print('error retrieving floodGrid')
         return 
     if (check_for_obstacle(data, node.position)):
         node.colour = 'black'
         floodGrid.insert(node)
-        print('found an obstacle')
+        # print('found an obstacle')
         return
     if (target_colour == replace_colour):
         return  
     if (floodGrid.get_node_at(node.position.x, node.position.y).colour != target_colour):
-        print('node is not target colour')
+        # print('node is not target colour')
         return
     if (not check_for_obstacle(data, get_position_to_left(node.position))):
         westNode = FloodNode(get_position_to_left(node.position), 'yellow')
-        print('westNode: ')
-        westNode.to_string()
+        # print('westNode: ')
+        # westNode.to_string()
         floodGrid.insert(westNode)
         flood_fill_left(westNode, target_colour, replace_colour)
     return
@@ -165,22 +165,22 @@ def flood_fill_left(node, target_colour, replace_colour):
 def flood_fill_right(node, target_colour, replace_colour):
     global floodGrid
     if (floodGrid is None):
-        print('error retrieving floodGrid')
+        # print('error retrieving floodGrid')
         return 
     if (check_for_obstacle(data, node.position)):
         node.colour = 'black'
         floodGrid.insert(node)
-        print('found an obstacle')
+        # print('found an obstacle')
         return
     if (target_colour == replace_colour):
         return  
     if (floodGrid.get_node_at(node.position.x, node.position.y).colour != target_colour):
-        print('node is not target colour')
+        # print('node is not target colour')
         return
     if (not check_for_obstacle(data, get_position_to_right(node.position))):
         eastNode = FloodNode(get_position_to_right(node.position), 'red')
-        print('eastNode:')
-        eastNode.to_string()
+        # print('eastNode:')
+        # eastNode.to_string()
         floodGrid.insert(eastNode)
         flood_fill_right(eastNode, target_colour, replace_colour)
     return
@@ -188,22 +188,22 @@ def flood_fill_right(node, target_colour, replace_colour):
 def flood_fill_up(node, target_colour, replace_colour):
     global floodGrid
     if (floodGrid is None):
-        print('error retrieving floodGrid')
+        # print('error retrieving floodGrid')
         return 
     if (check_for_obstacle(data, node.position)):
         node.colour = 'black'
         floodGrid.insert(node)
-        print('found an obstacle')
+        # print('found an obstacle')
         return
     if (target_colour == replace_colour):
         return  
     if (floodGrid.get_node_at(node.position.x, node.position.y).colour != target_colour):
-        print('node is not target colour')
+        # print('node is not target colour')
         return
     if (not check_for_obstacle(data, get_position_above(node.position))):
         northNode = FloodNode(get_position_above(node.position), 'blue')
-        print('northNode:')
-        northNode.to_string()
+        # print('northNode:')
+        # northNode.to_string()
         floodGrid.insert(northNode)
         flood_fill_up(northNode, target_colour, replace_colour)
     return
@@ -211,22 +211,22 @@ def flood_fill_up(node, target_colour, replace_colour):
 def flood_fill_below(node, target_colour, replace_colour):
     global floodGrid
     if (floodGrid is None):
-        print('error retrieving floodGrid')
+        # print('error retrieving floodGrid')
         return 
     if (check_for_obstacle(data, node.position)):
         node.colour = 'black'
         floodGrid.insert(node)
-        print('found an obstacle')
+        # print('found an obstacle')
         return
     if (target_colour == replace_colour):
         return  
     if (floodGrid.get_node_at(node.position.x, node.position.y).colour != target_colour):
-        print('node is not target colour')
+        # print('node is not target colour')
         return
     if (not check_for_obstacle(data, get_position_below(node.position))):
         southNode = FloodNode(get_position_below(node.position), 'green')
-        print('southNode:')
-        southNode.to_string()
+        # print('southNode:')
+        # southNode.to_string()
         floodGrid.insert(southNode)
         flood_fill_below(southNode, target_colour, replace_colour)
     return
